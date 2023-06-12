9716f3a91d600478d7189a0d329736700cd00f14
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-12 13:36:35 +0800

    renamed component

---
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Weapon
+{
+    public class WeaponController : MonoBehaviour
+    {
+        public Animator animator;
+
+        public AnimatorOverrideController overrideController;
+
+        public AnimationClip[] attackClips;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
