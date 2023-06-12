54aae1ca8c23ba37211960e3a846116433cb03d4
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-12 14:31:53 +0800

    added combo attacks and updated animation timings

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using EasyButtons;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ namespace Weapon
         public Animator animator;
 
         public AnimatorOverrideController overrideController;
-        
+
         /// <summary>
         /// Hitboxes (colliders) for attacks will be activated and stored inside of animation clips!.
         /// This allows for exact timing of when the colliders are enabled (and then disabled)
@@ -20,29 +21,66 @@ namespace Weapon
         /// The next attack in while in this period will be considered a consecutive attack (resulting in combo)
         /// In Seconds
         /// </summary>
-        public float ComboWaitPeriod_Secs= 0.5f;
+        public float ComboWaitPeriod_Secs = 0.5f;
+
         /// <summary>
         /// Number of combos
         /// </summary>
-        [HideInInspector]
-        public int ComboCounter = 0; 
-    
+        [ReadOnly(true)] public int ComboCounter = 0;
+
+        private float lastAttackTime = 0f;
+        private static readonly int AttackTrigger = Animator.StringToHash("Attack");
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            if (attackClips.Length == 0)
+            {
+                Debug.LogError("No Available Attack Clips!");
+            }
         }
 
         // Update is called once per frame
-        void Update()
+        void Update() { }
+
+
+        /// <summary>
+        /// Checks if the attack is consecutive and therefore results in a combo
+        /// </summary>
+        private void ExecuteComboCheck()
         {
-        
+            float currentAttackTime = Time.time;
+            // If time between last attack and now is lesser than wait period, it is a consecutive attack
+            bool isConsecutive = (currentAttackTime - lastAttackTime) < ComboWaitPeriod_Secs;
+            lastAttackTime = currentAttackTime;
+            if (isConsecutive) // If consecutive attack, increase combo counter
+            {
+                ComboCounter++;
+            }
+            else // Else reset combo
+            {
+                ComboCounter = 0;
+            }
         }
-        
+
+        /// <summary>
+        /// This function swaps out the current attack clip based on the current combo.
+        /// </summary>
+        private void SwapAttackClip()
+        {
+            int clipIndex = ComboCounter % attackClips.Length;
+            overrideController["BaseWeaponAttack"] = attackClips[clipIndex];
+        }
+
+        /// <summary>
+        /// Executes an attack. 
+        /// </summary>
         [Button]
         public void Attack()
         {
-            
+            SwapAttackClip();
+            animator.SetTrigger(AttackTrigger);
+            ExecuteComboCheck();
         }
     }
 }
