bd0d1da1a6b841f519ef3ba5cd6fc0fefad9cd8e
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-12 13:45:04 +0800

    the base stuff

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -7,8 +7,24 @@ namespace Weapon
         public Animator animator;
 
         public AnimatorOverrideController overrideController;
-
+        
+        /// <summary>
+        /// Hitboxes (colliders) for attacks will be activated and stored inside of animation clips!.
+        /// This allows for exact timing of when the colliders are enabled (and then disabled)
+        /// </summary>
         public AnimationClip[] attackClips;
+
+        /// <summary>
+        /// The amount of time to wait for.
+        /// The next attack in while in this period will be considered a consecutive attack (resulting in combo)
+        /// In Seconds
+        /// </summary>
+        public float ComboWaitPeriod_Secs= 0.5f;
+        /// <summary>
+        /// Number of combos
+        /// </summary>
+        [HideInInspector]
+        public int ComboCounter = 0; 
     
         // Start is called before the first frame update
         void Start()
@@ -20,6 +36,11 @@ namespace Weapon
         void Update()
         {
         
+        }
+        
+        public void Attack()
+        {
+            
         }
     }
 }
