52233dd90b0ab9783927ce21d6716edc74607258
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-13 13:38:00 +0800

    added gamemanager and saved scene as GameLevel.unity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Item;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public WeaponItem[] Weapons;
+    public SkillItem[] Skills;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
