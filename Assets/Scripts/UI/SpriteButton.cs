9e3ba538f431c21b60f937a887e1fa93ff60e905
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-18 18:48:49 +0800

    added sprite button

---
+++ Assets/Scripts/UI/SpriteButton.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Image))]
+    public class SpriteButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public Image image;
+        public Sprite neutral;
+        public Sprite hover;
+        public Sprite active;
+
+        private bool isHovering;
+        private bool isPressed;
+
+        private void UpdateImageSprite()
+        {
+            if (isPressed && isHovering)
+            {
+                image.sprite = active;
+            }
+            else if (isHovering)
+            {
+                image.sprite = hover;
+            }
+            else
+            {
+                image.sprite = neutral;
+            }
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isHovering = true;
+            UpdateImageSprite();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isHovering = false;
+            UpdateImageSprite();
+        }
+
+        
+        
+        private void Update()
+        {
+            bool last = isPressed;
+            isPressed = Input.GetKey(KeyCode.Mouse0);
+            if (isPressed != last)
+            {
+                UpdateImageSprite();
+            }
+
+        }
+    }
+}
