10760d3c0add7d75d6b12edafed62496da3a3325
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-02 16:36:45 +0800

    added EntityBody.cs

---
+++ Assets/Scripts/Entities/EntityBody.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Entities
+{
+    /// <summary>
+    /// This class represents the "body" / (virtual)physical conditions of an entity,
+    /// such as its health, stamina, strength etc.
+    /// Note that the values stored by the variables/attributes with _ prefixed only store the base values, it does not include effects from equipment, buffs, etc.
+    /// To get the actual values, use the properties or Get methods. 
+    /// </summary>
+    public class EntityBody : MonoBehaviour
+    {
+        public int _Health; // Health of entity
+        public int _Stamina; // Used for dashing
+        public int _Mana; // Used for casting spells
+        public int _Strength; // Increases physical damage
+        public int _Speed; // Movement speed
+        public int _Defense; // Reduces damage taken
+        
+        public int _CurrentHealth;
+        public int _CurrentStamina;
+        public int _CurrentMana;
+
+        public IStatusEffect[] StatusEffects; // Curent status effects on entity
+        
+        public void Damage(int amt)
+        {
+            _Health -= amt;
+        }
+        // todo include methods to get the actual values that includes effects from equipment, buffs, etc.
+    }
+}
