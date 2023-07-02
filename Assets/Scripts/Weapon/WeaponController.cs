acb0f854d985790b5f7ce1ebb616a2819b098999
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-02 17:17:52 +0800

    updated to use proposed weapon animation sysrtem

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -14,13 +14,8 @@ namespace Weapon
         public float attackingTravelSpeed=0.8f;
         public float RotationWhenIdle = 90f;
         public AnimatorOverrideController overrideController;
-
-        /// <summary>
-        /// Hitboxes (colliders) for attacks will be activated and stored inside of animation clips!.
-        /// This allows for exact timing of when the colliders are enabled (and then disabled)
-        /// </summary>
-        public AnimationClip[] attackClips;
-
+        
+        public int AttacksCount = 3; // Number of attacks for this weapon
         /// <summary>
         /// The amount of time to wait for.
         /// The next attack in while in this period will be considered a consecutive attack (resulting in combo)
@@ -35,13 +30,15 @@ namespace Weapon
 
         private float lastAttackTime = 0f;
         private static readonly int AttackTrigger = Animator.StringToHash("Attack");
+        private static readonly int AttackIndex = Animator.StringToHash("AttackIndex");
+
 
         // Start is called before the first frame update
         void Start()
         {
-            if (attackClips.Length == 0)
+            if (AttacksCount == 0)
             {
-                Debug.LogError("No Available Attack Clips!");
+                Debug.LogError("No Available Attacks!");
             }
         }
 
@@ -88,8 +85,9 @@ namespace Weapon
         /// </summary>
         private void SwapAttackClip()
         {
-            int clipIndex = ComboCounter % attackClips.Length;
-            overrideController["BaseWeaponAttack"] = attackClips[clipIndex];
+            int clipIndex = ComboCounter % (AttacksCount-1);
+            animator.SetInteger(AttackIndex, clipIndex);
+            // overrideController["BaseWeaponAttack"] = attackClips[clipIndex];
         }
 
 
