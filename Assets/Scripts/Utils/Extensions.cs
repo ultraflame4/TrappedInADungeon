37adf0f761213230be7b08bfcd210652a259c72c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-15 12:31:15 +0800

    fixed DestroyChildren not destroying all children

--- Assets/Scripts/Utils/Extensions.cs
+++ Assets/Scripts/Utils/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,36 +11,44 @@ namespace Utils
     {
         public static void DestroyChildren(this Transform transform)
         {
-            foreach (Transform child in transform)
+            List<Transform> children = transform.Cast<Transform>().ToList();
+            foreach (var child in children)
             {
+                if (Application.isEditor)
+                {
+                    GameObject.DestroyImmediate(child.gameObject);
+                    continue;
+                }
                 GameObject.Destroy(child.gameObject);
             }
         }
+
         public static void DestroyChildren(this GameObject gameObject)
         {
             gameObject.transform.DestroyChildren();
         }
-        
+
         /// <summary>
         /// Sets the sprite of an image, and automatically enables/disables the image if the sprite is null or not.
         /// </summary>
         /// <param name="image"></param>
         /// <param name="sprite"></param>
-        public static void SetSprite(this Image image, Sprite sprite=null)
+        public static void SetSprite(this Image image, Sprite sprite = null)
         {
             image.sprite = sprite;
             image.enabled = sprite != null;
         }
-        
+
         public static string JoinString(this IEnumerable<string> strings, string separator = ",")
         {
             return string.Join(separator, strings);
         }
+
         public static bool IsNegative(this float value)
         {
             return value < 0;
         }
-        
+
         public static T Add<T>(this List<T> a, T b)
         {
             a.Add(b);
