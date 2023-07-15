56fb15bb36bfb3b0c0ad4334911b74592ea51f97
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-15 13:20:59 +0800

    added parallax background stuff

--- Assets/Scripts/Level/ParallaxBackground.cs
+++ Assets/Scripts/Level/ParallaxBackground.cs
@@ -1,4 +1,3 @@
-using System;
 using EasyButtons;
 using UnityEngine;
 using Utils;
@@ -10,29 +9,68 @@ namespace Level
         public Sprite[] layers;
         public Color colorFrom = Color.white;
         public Color colorTo = Color.black;
+        public float parallaxFactor;
+        public int sections = 3;
+        public float yOffset;
         private GameObject[] layerObjects;
+        private Transform camera;
+
+        private Vector3 startPos;
 
         // Start is called before the first frame update
-        [Button("Generate Layers")]
+
         void Start()
+        {
+            startPos = transform.position;
+            camera = GameObject.FindWithTag("MainCamera").transform;
+            GenerateLayers();
+        }
+
+        [Button("Generate Layers")]
+        void GenerateLayers()
         {
             transform.DestroyChildren();
             layerObjects = new GameObject[layers.Length];
             for (var i = 0; i < layers.Length; i++)
             {
-                var sprite = layers[i];
-                GameObject gameObj = new($"Layer {i}");
-                layerObjects[i] = gameObj;
-                gameObj.transform.parent = transform;
-                gameObj.transform.localPosition = Vector3.zero;
-                var renderer = gameObj.AddComponent<SpriteRenderer>();
-                renderer.sprite = sprite;
-                renderer.color = Color.Lerp(colorFrom, colorTo, (float)i / (layers.Length - 1));
+                
+                GameObject layerObj = new($"Layer Container {i}");
+                layerObjects[i] = layerObj;
+                layerObj.transform.parent = transform;
+                layerObj.transform.localPosition = new Vector3(0,yOffset,-i);
+                for (int j = 0; j < sections; j++)
+                {
+                    GenerateSection(layerObj.transform,i,j);
+                }
+                
             }
         }
 
+        void GenerateSection(Transform parent, int layerIndex, int sectionIndex)
+        {
+            var sprite = layers[layerIndex];
+            GameObject gameObj = new($"Layer {layerIndex}");
+            gameObj.transform.parent = parent;
+            
+            gameObj.transform.localPosition = Vector3.right * sprite.bounds.size.x * sectionIndex;
+            var renderer = gameObj.AddComponent<SpriteRenderer>();
+            renderer.sprite = sprite;
+            renderer.color = Color.Lerp(colorFrom, colorTo, (float)layerIndex / (layers.Length - 1));
+        }
 
         // Update is called once per frame
-        void Update() { }
+        void Update()
+        {
+            if (camera == null) return;
+            float travelX = (camera.position - startPos).x;
+            for (var i = 0; i < layerObjects.Length; i++)
+            {
+                var layer = layerObjects[i];
+                float layerPercent = (float)i / (layerObjects.Length - 1);
+                Vector3 layerPos = layer.transform.position;
+                layerPos.x = startPos.x + travelX * layerPercent * parallaxFactor;
+                layer.transform.position = layerPos;
+            }
+        }
     }
 }
