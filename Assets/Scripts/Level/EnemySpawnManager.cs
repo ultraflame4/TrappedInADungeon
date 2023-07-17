93aa00ab6938c7caf91ad777ca965a401ad35a5d
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-17 11:35:49 +0800

    Added generation of spawn sections

--- Assets/Scripts/Level/EnemySpawnManager.cs
+++ Assets/Scripts/Level/EnemySpawnManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using EasyButtons;
 using Level;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Utils;
+using Random = UnityEngine.Random;
 
 namespace Enemies
 {
@@ -10,7 +13,8 @@ namespace Enemies
     public class EnemySpawnManager : MonoBehaviour
     {
         public LevelManager levelManager;
-        [Tooltip("Enemy spawning is divided into sections. This determines how big each section is."),Min(0.1f)]
+
+        [Tooltip("Enemy spawning is divided into sections. This determines how big each section is."), Min(0.1f)]
         public float spawnSectionSize = 10f;
 
         public float difficultyLevel = 1f;
@@ -19,28 +23,52 @@ namespace Enemies
         /// Number of spawn sections in the level.
         /// </summary>
         public int SectionsCount => Mathf.FloorToInt(levelManager.levelSize / spawnSectionSize);
-        
-        
-        public Rect[] GetSpawnSections()
+
+        private void Start()
         {
-            Vector2 spawnSectionOffset = Vector2.left * (levelManager.levelSize / 2 - spawnSectionSize/2);
+            GenerateSpawnSections();
+        }
+
+        private Vector2[] GetSpawnSectionPositions()
+        {
+            Vector2 spawnSectionOffset = Vector2.left * (levelManager.levelSize / 2 - spawnSectionSize / 2);
             Vector2 gap = (levelManager.levelSize - SectionsCount * spawnSectionSize) / (SectionsCount + 1) * Vector2.left;
-            var spawnSections = new Rect[SectionsCount];
+            var spawnSections = new Vector2[SectionsCount];
             for (int i = 0; i < SectionsCount; i++)
             {
-                Vector2 position = (Vector2)transform.position + new Vector2(i * spawnSectionSize, 0) + spawnSectionOffset - gap * (i+1);
-                spawnSections[i] = new Rect(position, new Vector2(spawnSectionSize, 1));
+                spawnSections[i] = (Vector2)transform.position + new Vector2(i * spawnSectionSize, 0) + spawnSectionOffset - gap * (i + 1);
             }
+
             return spawnSections;
+        }
+
+        [Button]
+        public void GenerateSpawnSections()
+        {
+            var container = GameObject.Find("SpawnSections");
+            if (container is null)
+            {
+                container = new GameObject("SpawnSections");
+                container.transform.SetParent(transform);
+            }
+            container.DestroyChildren();
+            foreach (Vector2 section in GetSpawnSectionPositions())
+            {
+                Vector2 randomPosition = Vector2.right * (spawnSectionSize / 4 * Random.Range(-1, 1));
+                var sectionObj = new GameObject("SpawnSection");
+                sectionObj.transform.SetParent(container.transform);
+                sectionObj.transform.position = section + randomPosition;
+                sectionObj.AddComponent<EnemySpawner>();
+            }
         }
 
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
 
-            foreach (Rect section in GetSpawnSections())
+            foreach (Vector2 section in GetSpawnSectionPositions())
             {
-                Gizmos.DrawWireCube(section.position, new Vector3(spawnSectionSize, 20, 1));
+                Gizmos.DrawWireCube(section, new Vector3(spawnSectionSize, 1, 1));
             }
         }
     }
