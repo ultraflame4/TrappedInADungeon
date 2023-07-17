562e3d5236070639d637ea2f8359b6cd3ff274ac
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-17 17:13:47 +0800

    teleport player to start of level

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -12,22 +12,26 @@ namespace Level
         public CinemachineConfiner2D cameraConfiner;
         private Ground ground;
         private ParallaxBackground background;
-        private EnemySpawnManager enemySpawnManager; 
+        private EnemySpawnManager enemySpawnManager;
+        private Transform player;
 
         [field: SerializeField]
         public float levelSize { get; private set; } = 10f;
 
         [field: SerializeField]
         public float levelHeight { get; private set; } = 30f;
+
         [field: SerializeField]
-        public float groundLevel  {get; private set; }  = 1f;
-        public Vector2 LevelLeft => new Vector2(-levelSize/2, 0);
-        
+        public float groundLevel { get; private set; } = 1f;
+
+        public Vector2 LevelLeft => new Vector2(-levelSize / 2, 0);
+
         private void Start()
         {
+            player = GameObject.FindWithTag("Player").transform;
             GenerateLevel();
         }
-        
+
         /// <summary>
         /// Retrieves the required components for the level manager.
         /// </summary>
@@ -40,18 +44,23 @@ namespace Level
             if (background is null) throw new NullReferenceException("Could not find ParallaxBackground component in children of LevelManager");
             if (ground is null) throw new NullReferenceException("Could not find Ground component in children of LevelManager");
         }
-        
+
         [Button]
         public void GenerateLevel()
         {
             GetRequiredComponents();
             background.sections = Mathf.CeilToInt(levelSize / background.SectionWidth) + 1;
-            background.xOffset = -(background.TotalWidth-background.SectionWidth) / 2;
+            background.xOffset = -(background.TotalWidth - background.SectionWidth) / 2;
             background.GenerateLayers();
             ground.UpdateWidth(levelSize);
             enemySpawnManager.GenerateSpawnSections();
             GenerateColliderBounding();
+            if (player is not null) // Transport player to start of level. todo change this later to level start
+            {
+                player.transform.position = LevelLeft + Vector2.right + Vector2.up*groundLevel + Vector2.up;
+            }
         }
+
         /// <summary>
         /// Generates the bounding for the camera confiner (which uses a polygon collider 2D as bounding).
         /// Also generates the edge colliders to block player.
@@ -68,7 +77,7 @@ namespace Level
 
             poly.enabled = false;
             edge.enabled = true;
-            poly.points =  new[] {
+            poly.points = new[] {
                     new Vector2(LevelLeft.x, levelHeight),
                     new Vector2(LevelLeft.x + levelSize, levelHeight),
                     new Vector2(LevelLeft.x + levelSize, 0),
@@ -90,14 +99,13 @@ namespace Level
             yield return new WaitForSeconds(0.1f);
             cameraConfiner.InvalidateCache();
         }
-        
+
         private void OnDrawGizmos()
         {
-            Gizmos.color = Color.yellow+Color.red;
-            Gizmos.DrawWireCube(transform.position+ new Vector3(0, groundLevel), new Vector3(levelSize, 0));
+            Gizmos.color = Color.yellow + Color.red;
+            Gizmos.DrawWireCube(transform.position + new Vector3(0, groundLevel), new Vector3(levelSize, 0));
             Gizmos.color = Color.cyan;
-            Gizmos.DrawWireCube(transform.position+ new Vector3(0, levelHeight/2), new Vector3(levelSize, levelHeight));
-            
+            Gizmos.DrawWireCube(transform.position + new Vector3(0, levelHeight / 2), new Vector3(levelSize, levelHeight));
         }
     }
 }
