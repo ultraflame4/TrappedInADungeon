558d4a1f15006fe81614fb998b196338490ff34c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-17 16:03:48 +0800

    GenerateLevel now also updates generate spawn sections + testing camera confiner

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using Cinemachine;
 using EasyButtons;
 using UnityEngine;
 
 namespace Level
 {
+    [RequireComponent(typeof(EnemySpawnManager))]
     public class LevelManager : MonoBehaviour
     {
-        public Ground ground;
+        public CinemachineConfiner2D cameraConfiner;
+        private Ground ground;
         private ParallaxBackground background;
+        private EnemySpawnManager enemySpawnManager; 
 
         [field: SerializeField]
         public float levelSize { get; private set; } = 10f;
@@ -29,6 +33,7 @@ namespace Level
         {
             background = GetComponentInChildren<ParallaxBackground>();
             ground = GetComponentInChildren<Ground>();
+            enemySpawnManager = GetComponent<EnemySpawnManager>();
             if (background is null) throw new NullReferenceException("Could not find ParallaxBackground component in children of LevelManager");
             if (ground is null) throw new NullReferenceException("Could not find Ground component in children of LevelManager");
         }
@@ -41,6 +46,26 @@ namespace Level
             background.xOffset = -(background.TotalWidth-background.SectionWidth) / 2;
             background.GenerateLayers();
             ground.UpdateWidth(levelSize);
+            enemySpawnManager.GenerateSpawnSections();
+            GenerateColliderBounding();
+        }
+
+        void GenerateColliderBounding()
+        {
+            PolygonCollider2D collider = GetComponent<PolygonCollider2D>();
+            if (collider is null)
+            {
+                collider = gameObject.AddComponent<PolygonCollider2D>();
+            }
+
+            collider.enabled = false;
+            collider.points = new[] {
+                    new Vector2(LevelLeft.x, 20),
+                    new Vector2(LevelLeft.x + levelSize, 20),
+                    new Vector2(LevelLeft.x + levelSize, -20),
+                    new Vector2(LevelLeft.x, -20),
+            };
+            cameraConfiner.InvalidateCache();
         }
         
         private void OnDrawGizmos()
