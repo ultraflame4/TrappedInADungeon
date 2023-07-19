fad2e5579b54ad9f10d3df397d1ecb07f12f547c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-19 21:32:05 +0800

    added cool particle effect when player goes nears the portal

--- Assets/Scripts/Level/InteractableObject.cs
+++ Assets/Scripts/Level/InteractableObject.cs
@@ -3,36 +3,36 @@ using UnityEngine;
 
 namespace Level
 {
-    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(CircleCollider2D))]
     public class InteractableObject : MonoBehaviour
     {
 
         [Tooltip("The text to show when the player is in the interactable zone. ( When the player is inside the boxCollider trigger)"),Multiline]
         public string popupText = "No text set for this interaction!";
         
-        private BoxCollider2D boxCollider2D;
+        private CircleCollider2D circleCollider2D;
         /// <summary>
         /// Invoked when player enters or leave the interactable zone;
         /// value is true when player enters the interactable zone, false when  player exits.
         /// </summary>
-        public event Action<bool> InteractableZoneEnter; 
+        public event Action<bool> InteractableChange; 
         public bool interactableZoneActive { get; private set; }
         private void Start()
         {
-            boxCollider2D = GetComponent<BoxCollider2D>();
+            circleCollider2D = GetComponent<CircleCollider2D>();
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.gameObject.CompareTag("Player")) return;
             interactableZoneActive = true;
-            InteractableZoneEnter?.Invoke(true);
+            InteractableChange?.Invoke(true);
         }
         private void OnTriggerExit2D(Collider2D other)
         {
             if (!other.gameObject.CompareTag("Player")) return;
             interactableZoneActive = false;
-            InteractableZoneEnter?.Invoke(false);
+            InteractableChange?.Invoke(false);
         }
     }
 }
---
+++ Assets/Scripts/PortalInteraction.cs
@@ -0,0 +1,28 @@
+﻿using Level;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem)), RequireComponent(typeof(InteractableObject))]
+public class PortalInteraction : MonoBehaviour
+{
+    public float initialRadialMulti = 0.5f;
+    public float activeRadialMulti = 1f;
+    public float initialEmissionRate = 5f;
+    public float activeEmissionRate = 15f;
+    private InteractableObject interactableObject;
+    private ParticleSystem particleSys;
+
+    private void Start()
+    {
+        particleSys = GetComponent<ParticleSystem>();
+        interactableObject = GetComponent<InteractableObject>();
+        interactableObject.InteractableChange += OnInteractableChange;
+    }
+
+    void OnInteractableChange(bool value)
+    {
+        ParticleSystem.VelocityOverLifetimeModule velOverLifetime = particleSys.velocityOverLifetime;
+        ParticleSystem.EmissionModule emissionModule = particleSys.emission;
+        velOverLifetime.radialMultiplier = value ? activeRadialMulti : initialRadialMulti;
+        emissionModule.rateOverTime = value ? activeEmissionRate : initialEmissionRate;
+    }
+}
