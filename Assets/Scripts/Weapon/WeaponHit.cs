cba40d6188a2ab277b72b16c798280e90a793b29
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-22 14:29:35 +0800

    weapon damage now uses PlayerBody.CalculateAttackDamage

--- Assets/Scripts/Weapon/WeaponHit.cs
+++ Assets/Scripts/Weapon/WeaponHit.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using Entities;
+using Player;
 using UnityEngine;
 
 namespace Weapon
 {
+    [RequireComponent(typeof(WeaponController))]
     public class WeaponHit : MonoBehaviour
     {
-        public WeaponController controller;
+        private PlayerBody playerBody;
+        private WeaponController controller;
+        private void Start()
+        {
+            playerBody = GameObject.FindWithTag("Player").GetComponent<PlayerBody>();
+            controller = GetComponent<WeaponController>();
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.gameObject.CompareTag("Enemy")) return;
             EntityBody body = other.gameObject.GetComponent<EntityBody>();
             if (body is null) return;
-            body.Damage(controller.weaponItem.Attack);
+            body.Damage(playerBody.CalculateAttackDamage(controller.weaponItem.Attack));
         }
         
     }
