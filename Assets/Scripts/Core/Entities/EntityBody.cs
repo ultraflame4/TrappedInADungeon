dc39ff322ce8f74a982b402ddb929025ca1c944d
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-24 18:46:56 +0800

    updated stats

--- Assets/Scripts/Core/Entities/EntityBody.cs
+++ Assets/Scripts/Core/Entities/EntityBody.cs
@@ -66,7 +66,7 @@ namespace Core.Entities
         /// <returns></returns>
         public float CalculateAttackDamage(float baseDamage)
         {
-            return baseDamage + Attack;
+            return baseDamage*Level + Attack;
         }
     }
 }
