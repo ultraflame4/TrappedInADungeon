ede366b5fc53ae4df91359b76aeacbdc51d6c911
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-24 19:07:03 +0800

    updated how consumables work & player stats

--- Assets/Scripts/Consumable/ConsumableItemController.cs
+++ Assets/Scripts/Consumable/ConsumableItemController.cs
@@ -10,8 +10,8 @@ namespace Consumable
 
         protected override void OnItemUse()
         {
-            playerBody.CurrentHealth.value += itemInstance.item.health;
-            playerBody.CurrentMana.value += itemInstance.item.mana;
+            playerBody.CurrentHealth.value += itemInstance.item.health/100*playerBody.Health;
+            playerBody.CurrentMana.value += itemInstance.item.mana/100*playerBody.Mana;
             gateway.PlayerInventory.AdjustItemCount(itemInstance,itemInstance.Count-1);
         }
     }
