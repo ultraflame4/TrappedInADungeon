60cd88d9f4d49af2b4aa09ad76517bb960988baf
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-07-27 14:51:04 +0800

    fixed weird resolution when transitioning between scenes

--- Assets/Scripts/UI/SceneTransition/SceneTransitionController.cs
+++ Assets/Scripts/UI/SceneTransition/SceneTransitionController.cs
@@ -10,12 +10,12 @@ namespace UI.SceneTransition
     [RequireComponent(typeof(Image))]
     public class SceneTransitionController : MonoBehaviour
     {
-
         public TransitionEffect effectA;
         public TransitionEffect effectB;
         public TextMeshProUGUI loadingText;
         public Image logo;
         private Image image;
+
         private void Start()
         {
             image = GetComponent<Image>();
@@ -28,14 +28,15 @@ namespace UI.SceneTransition
         IEnumerator FadeOutCoroutine()
         {
             loadingText.gameObject.SetActive(true);
-            logo.gameObject.SetActive(false);
+            logo.gameObject.SetActive(true);
             yield return effectB.FadeToBlackCoroutine();
             loadingText.gameObject.SetActive(false);
             logo.gameObject.SetActive(false);
             effectB.ClearOut();
             yield return effectA.FadeToClearCoroutine();
             image.raycastTarget = false; // Disable so that the player can interact with ui
-        } 
+        }
+
         IEnumerator FadeInCoroutine()
         {
             image.raycastTarget = true; // Block ui interaction
@@ -44,27 +45,30 @@ namespace UI.SceneTransition
             loadingText.gameObject.SetActive(true);
             logo.gameObject.SetActive(true);
             yield return effectB.FadeToClearCoroutine();
-            
         }
 
         public void FadeOut()
         {
             StartCoroutine(FadeOutCoroutine());
         }
-        
+
         public IEnumerator TransitionToSceneCoroutine(string sceneName)
         {
             yield return null;
             yield return FadeInCoroutine();
             AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
-            
+            asyncOperation.allowSceneActivation = false;
             while (!asyncOperation.isDone)
             {
-                loadingText.text = $"Loading - {asyncOperation.progress*100}%";
+                loadingText.text = $"Loading - {asyncOperation.progress * 100}%";
+                if (asyncOperation.progress >= 0.9f) break;
                 yield return null;
             }
+
+            asyncOperation.allowSceneActivation = true;
             yield return null;
         }
+
         /// <summary>
         /// Loads & transitions to a scene with the given name.
         /// </summary>
