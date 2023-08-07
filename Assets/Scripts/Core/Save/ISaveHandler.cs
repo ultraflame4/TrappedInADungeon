132e119c6e1a5481a59ce57f4d6c5f74d816931e
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-08-07 15:21:22 +0800

    comment

--- Assets/Scripts/Core/Save/ISaveHandler.cs
+++ Assets/Scripts/Core/Save/ISaveHandler.cs
@@ -2,11 +2,12 @@
 
 namespace Core.Save
 {
-    [JsonObject(MemberSerialization.OptIn)]
+    [JsonObject(MemberSerialization.OptIn)] // Only serialise members (in child) with JsonProperty attribute
     public interface ISaveHandler
     {
         public void OnLoadSave(string json)
         {
+            // Override all data (fields with JsonProperty attribute) in this instance with data from json
             JsonConvert.PopulateObject(json,this);
         }
 
@@ -16,6 +17,7 @@ namespace Core.Save
         /// <returns></returns>
         public string OnWriteSave()
         {
+            // Serialise all data (fields with JsonProperty attribute) in this instance to json
             return JsonConvert.SerializeObject(this);
         }
         
