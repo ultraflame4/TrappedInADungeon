313f434e979fd3fae8900c18f993a1e1ca665113
ultr42 <ultr42@skiff.com>
2023-08-10 19:18:03 +0800

    comments + removed useless line of code

--- Assets/Scripts/UI/Inventory/InventoryPanel.cs
+++ Assets/Scripts/UI/Inventory/InventoryPanel.cs
@@ -13,38 +13,47 @@ namespace UI.Inventory
     {
         [Tooltip("Scrollable content for the weapon list")]
         public Transform WeaponListContent;
+
         [Tooltip("Scrollable content for the skill list")]
         public Transform SkillListContent;
+
         [Tooltip("Scrollable content for the item list")]
         public Transform ItemListContent;
+
         [Tooltip("Prefab for the list item")]
         public GameObject ListItemPrefab;
-        
-        
+
+
         private InventoryListItem[] listItems;
         public static InventoryPanel Instance { get; private set; }
+
         private void Awake()
         {
+            // Listen for inventory updates
             Player.Inventory.InventoryUpdate += UpdateItemLists;
-                    
+
             if (Instance != null)
             {
                 Debug.LogError("Warning: multiple instances of InventoryPanel found! The static instance will be changed to this one!!!! This is probably not what you want!");
             }
+
             Instance = this;
-            GameSaveManager.AddSaveHandler("ui.inventory",this);
         }
 
         void UpdateItemLists()
         {
+            // On item list update, destroy all children and repopulate
             WeaponListContent.DestroyChildren();
             SkillListContent.DestroyChildren();
             ItemListContent.DestroyChildren();
+            // New array to store inventory list items
             listItems = new InventoryListItem[Player.Inventory.AllItems.Count];
+            // Loop over all items in the inventory and create a inventory list item for each
             for (var i = 0; i < Player.Inventory.AllItems.Count; i++)
             {
                 var instance = Player.Inventory.AllItems[i];
                 GameObject item;
+                // Create the inventory list item with the correct parent according to the item type.
                 switch (instance.item.itemType)
                 {
                     case ItemType.Weapon:
@@ -58,6 +67,7 @@ namespace UI.Inventory
                         break;
                 }
 
+                // Configure the list item with the correct item instance
                 var listItem = item.GetComponent<InventoryListItem>();
                 listItem.SetItem(instance);
                 listItems[i] = listItem;
@@ -65,12 +75,21 @@ namespace UI.Inventory
         }
 
 
+        /// <summary>
+        /// Returns the currently focused item instance in the inventory
+        /// </summary>
+        /// <returns></returns>
         public InvSlotItemInstance GetFocused()
         {
             if (!enabled) return null;
             return listItems.FirstOrDefault(x => x.itemInstance.focused)?.itemInstance;
         }
 
+        /// <summary>
+        /// Returns the item instance at the given index in the inventory
+        /// </summary>
+        /// <param name="itemIndex"></param>
+        /// <returns></returns>
         public InvSlotItemInstance GetInvItemByIndex(int itemIndex)
         {
             if (itemIndex < 0) return null;
