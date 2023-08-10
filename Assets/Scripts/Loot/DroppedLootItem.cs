d887bdd55ba7355c9bf7526c9e2a1272cdf45f63
ultr42 <ultr42@skiff.com>
2023-08-10 18:13:27 +0800

    added more comments

--- Assets/Scripts/Loot/DroppedLootItem.cs
+++ Assets/Scripts/Loot/DroppedLootItem.cs
@@ -6,17 +6,30 @@ using UnityEngine;
 
 namespace Loot
 {
+    /// <summary>
+    /// Controller for items dropped on the ground
+    /// </summary>
     [RequireComponent(typeof(SpriteRenderer))]
     public class DroppedLootItem : MonoBehaviour
     {
+        // component reference
         private SpriteRenderer spriteRenderer;
+        /// <summary>
+        /// The item this will give to the player
+        /// </summary>
         public ItemInstance itemInstance { get; private set; }
         private void Start()
         {
+            // Get component reference
             spriteRenderer = GetComponent<SpriteRenderer>();
+            // Set sprite of the prefab to the item sprite
             spriteRenderer.sprite = itemInstance.sprite;
         }
-
+        
+        /// <summary>
+        /// Used to set the item instance for this dropped item prefab
+        /// </summary>
+        /// <param name="item"></param>
         public void SetItem(ItemInstance item)
         {
             itemInstance = item;
--- Assets/Scripts/Loot/ExpBall.cs
+++ Assets/Scripts/Loot/ExpBall.cs
@@ -13,25 +13,32 @@ namespace Loot
 
         private void Start()
         {
-            StartCoroutine(MoveToPlayer());
+            // Only start moving to the player after delay.
+            StartCoroutine(StartMoveToPlayer());
         }
 
-        IEnumerator MoveToPlayer()
+        IEnumerator StartMoveToPlayer()
         {
+            // Only start moving to the player after delay.
             yield return new WaitForSeconds(moveDelaySecs);
             move = true;
         }
         private void FixedUpdate()
         {
+            // if waiting for delay, return
             if (!move) return;
+            // Linearly interpolate to the player position
             transform.position = Vector3.Lerp(transform.position,Player.Transform.position , 0.1f);
         }
 
 
         private void OnTriggerEnter2D(Collider2D other)
         {
+            // If not player, skip
             if (!other.CompareTag("Player")) return;
+            // When player enters the collider, player absorbs the exp. So add to the player
             Player.Body.AddExperiencePoints(expValue);
+            // Destroy gameObject after absorbed by player
             Destroy(gameObject);
         }
     }
